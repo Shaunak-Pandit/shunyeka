e9cb838223e3111a8ec0a95848297fcb757d60ba
Shaunak-Pandit <66392213+Shaunak-Pandit@users.noreply.github.com>
2022-06-29 20:18:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_lambda_function" "test_lambda" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("hello-python.zip")
+  source_code_hash = filebase64sha256("${path.module}/python/hello-python.zip")
 
   runtime = "nodejs12.x"
 
