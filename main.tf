1c406148b24c37826dc4836ce47cd06aecb9398b
Shaunak-Pandit <66392213+Shaunak-Pandit@users.noreply.github.com>
2022-06-29 20:06:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,50 +11,6 @@ resource "aws_s3_bucket" "my-s3-bucket" {
   
   tags = var.tags
 }
-resource "aws_iam_role" "lambda_role" {
-name   = "shunyEka_Lambda_Function_Role"
-assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "lambda.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
-}
-resource "aws_iam_policy" "iam_policy_for_lambda" {
- 
- name         = "aws_iam_policy_for_terraform_aws_lambda_role"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": [
-       "logs:CreateLogGroup",
-       "logs:CreateLogStream",
-       "logs:PutLogEvents"
-     ],
-     "Resource": "arn:aws:logs:*:*:*",
-     "Effect": "Allow"
-   }
- ]
-}
-EOF
-}
-resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
- role        = aws_iam_role.lambda_role.name
- policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
-}
 
 data "archive_file" "zip_the_python_code" {
 type        = "zip"
@@ -62,11 +18,24 @@ source_dir  = "${path.module}/python/"
 output_path = "${path.module}/python/hello-python.zip"
 }
 
-resource "aws_lambda_function" "terraform_lambda_func" {
-filename                       = "${path.module}/python/hello-python.zip"
-function_name                  = "shunyEka_Lambda_Function"
-role                           = aws_iam_role.lambda_role.arn
-handler                        = "index.lambda_handler"
-runtime                        = "python3.8"
-depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a 
+  # path.module in the filename.
+  filename      = "hello-python.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.cloudformation_s3_lambda_role.arn
+  handler       = "index.test"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("hello-python.zip")
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
 }
