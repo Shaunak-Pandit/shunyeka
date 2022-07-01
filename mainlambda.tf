78e0b24ed206e61eeff2195a75a2fe7227813ab5
shaunakpandit <66392213+Shaunak-Pandit@users.noreply.github.com>
2022-07-02 00:37:59 +0530

    Update mainlambda.tf

--- mainlambda.tf
+++ mainlambda.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+resource "aws_iam_role" "lambda_role" {
+  name = "terraform_aws_lambda_role"
 
   assume_role_policy = <<EOF
 {
@@ -21,22 +21,49 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 EOF
 }
-locals{
-  lambda_zip_location = "outputs/welcome.zip"
+
+resource "aws_iam_policy" "iam_policy_for_lambda"{
+    name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+    path        ="/"
+    description = "Policy to manage lambda role"
+    policy= <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF  
 }
 
-data "archive_file" "welcome" {
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+    role        = aws_iam_role.lambda_role.name
+    policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn  
+} 
+
+data "archive_file" "zip_the_python_code" {
   type        = "zip"
-  source_file = "welcome.py"
-  output_path = "${local.lambda_zip_location}"
+  source_file = "${path.module}/python/"
+  output_path = "${path.module}/python/hello-python.zip"
 }
 
-resource "aws_lambda_function" "test_lambda" {
-  filename         = "${local.lambda_zip_location}"
-  role             = aws_iam_role.iam_for_lambda.arn
-  function_name    = "welcome"
-  handler          = "welcome.hello"
-  runtime          = "python3.7"
-  timeout          = 180
-  source_code_hash = filebase64sha256("${local.lambda_zip_location}")
+resource "aws_lambda_function" "terraform_lambda_func" {
+  filename         = "${path.module}/python/hello-python.zip"
+  function_name    = "shunyeka-function"
+  role             = aws_iam_role.lambda_role.arn 
+  handler          = "hello-python.lambda_handler"
+  runtime          = "python3.8"
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role] 
+}
+
+output "terraform_aws_role_output"{
+    value = aws_iam_role.lambda_role.name
 }
