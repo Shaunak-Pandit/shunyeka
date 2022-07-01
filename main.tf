34bbfae91bcb854094a76f129b01426c157e97e6
shaunakpandit <66392213+Shaunak-Pandit@users.noreply.github.com>
2022-07-01 23:58:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,42 +13,5 @@ resource "aws_s3_bucket" "my-s3-bucket" {
   tags = var.tags
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-locals{
-  lambda_zip_location = "outputs/welcome.zip"
-}
-
-data "archive_file" "welcome" {
-  type        = "zip"
-  source_file = "welcome.py"
-  output_path = "${local.lambda_zip_location}"
-}
-
-resource "aws_lambda_function" "test_lambda" {
-  filename         = "${local.lambda_zip_location}"
-  role             = aws_iam_role.iam_for_lambda.arn
-  function_name    = "welcome"
-  handler          = "welcome.hello"
-  runtime          = "python3.7"
-  timeout          = 180
-  source_code_hash = filebase64sha256("${local.lambda_zip_location}")
-}
 
